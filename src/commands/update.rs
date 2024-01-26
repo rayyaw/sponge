271dba73f81c2160b6088b9f451a161e1921d573
rayyaw <rayyawspeedruns@gmail.com>
2024-01-26 12:03:05 -0600

    Basic version of update all API complete

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -1,4 +1,7 @@
+use std::ffi::CStr;
+
 use dlopen2::wrapper::Container;
+use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::api::PackageManagerApi;
 
@@ -11,8 +14,39 @@ pub fn update(args: Vec<String>, plugins: Vec<Container<PackageManagerApi>>) {
 }
 
 fn update_all(plugins: Vec<Container<PackageManagerApi>>) {
-    // TODO - Implement
+    // Setup progress bar
+    let progress_style = ProgressStyle::default_bar()
+        .template("[{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+        .expect("Unable to create progress bar!");
+
+    // Set up a progress bar
+    let progress_bar = ProgressBar::new(plugins.len().try_into().unwrap());
+    progress_bar.set_style(progress_style);
+
     for plugin in plugins {
-        println!("{:?}", plugin.plugin_name());
+        // Get the plugin name
+        let plugin_name_raw = plugin.plugin_name();
+        let plugin_name = unsafe { CStr::from_ptr(plugin_name_raw) }.to_string_lossy();
+
+        // Get the API version, then call the corresponding function
+        let api_version = plugin.upgrade_all_api_version();
+
+        // Call the correct API version
+        match api_version {
+            1 => {
+                plugin.upgrade_all_api_v1().expect(&format!(
+                    "API Error: Could not find the upgrade all API v1 of {}!", plugin_name
+                )) 
+            }
+
+            _ => { panic!(
+                "API Error: The upgrade all API of {} uses undefined version {}!",
+                plugin_name,
+                api_version,
+            )}
+        };
+
+        // Increment the progress bar
+        progress_bar.inc(1);
     }
 }
